9a29597eccd002e5ec5b8111881bbc5603d4eac6
naomi <glamourbeautyus@gmail.com>
2022-12-19 21:22:34 +0000

    Maven Note

---
+++ Maven.sh
@@ -0,0 +1,16 @@
+https://github.com/Unaomi/Packagement.git
+Get stated by
+1.	 Create a new repository on the command line
+echo "# Packagement" >> README.md
+]git init
+]git add README.md
+]git commit -m "first commit"
+]git branch -M main
+git remote add origin https://github.com/Unaomi/Packagement.git
+git push -u origin main
+
+2.	To push an existing repository from the command line
+]git remote add origin https://github.com/Unaomi/Packagement.git
+git branch -M main
+git push -u origin main
+
